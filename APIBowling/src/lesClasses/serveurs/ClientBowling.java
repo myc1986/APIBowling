b4a6ed5071212907f5c9eb97f7e463e001d2dbcd
myc1986 <moratinos.c@gmail.com>
2014-05-23 00:58:07 +0200

    Ajout de la communication Client/Serveur par socket

---
+++ APIBowling/src/lesClasses/serveurs/ClientBowling.java
@@ -0,0 +1,29 @@
+package lesClasses.serveurs;
+
+import java.io.BufferedReader;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientBowling {
+
+	Socket maSocketClient;
+	
+	public static void main(String[] args) {
+		Socket uneSocket;
+		BufferedReader unBufferReader;
+		
+		try {
+			uneSocket = new Socket(InetAddress.getLocalHost(), 2009);
+			System.out.println("Demande de connexion");
+			
+			uneSocket.close();
+		} catch (UnknownHostException e) {
+			
+			e.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ APIBowling/src/lesClasses/serveurs/ServeurBowling.java
@@ -0,0 +1,38 @@
+package lesClasses.serveurs;
+
+import java.io.IOException;
+import java.io.ObjectInputStream.GetField;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServeurBowling {
+
+	public static void main(String[] args) {
+		ServerSocket uneSocketServeur;
+		Socket socketServeur;
+		
+		try {
+			System.out.println("Démarrage d'écoute du serveur Bowling...");
+			
+			uneSocketServeur = new ServerSocket(2009);
+			
+			System.out.println("Serveur Bowling en écoute");
+			
+			socketServeur = uneSocketServeur.accept();
+			
+			System.out.println("Connexion établie");
+			
+			uneSocketServeur.close();
+			socketServeur.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+	}
+
+}
