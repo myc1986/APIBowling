cc19b6f5b1da7b28449a785f446c525d8d4a82cb
myc1986 <moratinos.c@gmail.com>
2014-05-23 11:33:55 +0200

    ajout de nouvelles méthodes Interface IClientSocketBowling 	- ConfirmerTicketReservation 	- ObtenirScores
    interface IServeurSocketBowling
    	- ConfirmerTicketReservation
    	- ObtenirScores

--- APIBowling/src/lesInterfaces/IClientSocketBowling.java
+++ APIBowling/src/lesInterfaces/IClientSocketBowling.java
@@ -1,5 +1,13 @@
 package lesInterfaces;
 
-public interface IClientSocketBowling {
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
 
+import lesClasses.Reservation;
+
+public interface IClientSocketBowling {
+	void main(String[] args);
+	String ObtenirTicketReservation(ArrayList<IJoueur> lesJoueurs);
+	Reservation ConfirmerTicketReservation(Reservation maReservation);
+	Reservation ObtenirScores(String codeReservation);
 }
--- APIBowling/src/lesInterfaces/IServeurSocketBowling.java
+++ APIBowling/src/lesInterfaces/IServeurSocketBowling.java
@@ -1,5 +1,12 @@
 package lesInterfaces;
 
-public interface IServeurSocketBowling {
+import java.util.ArrayList;
 
+import lesClasses.Reservation;
+
+public interface IServeurSocketBowling {
+	void main(String[] args);
+	String ObtenirTicketReservation(ArrayList<IJoueur> lesJoueurs);
+	Reservation ConfirmerTicketReservation(Reservation maReservation);
+	Reservation ObtenirScores(String codeReservation);
 }
