11e35708a77b46b0ec244b4f03fae47263d2ba2b
myc1986 <moratinos.c@gmail.com>
2014-05-26 02:52:04 +0200

    Création interface IBddOracle. Cette interface définit les requêtes possibles avec une base orcacle pour l'application de bowling.

---
+++ APIBowling/src/lesInterfaces/IBddOracle.java
@@ -0,0 +1,18 @@
+package lesInterfaces;
+
+import java.util.ArrayList;
+
+import lesClasses.Reservation;
+
+public interface IBddOracle {
+	
+	ArrayList<Reservation> GetListReservations();
+	Reservation GetReservation(String codeReservation);
+	IPiste GetPisteAvecMaxTempsDisponibilite();
+	ArrayList<IFrame> GetListFrameJoueur(String idJoueur);
+	ArrayList<IJoueur> GetListJoueurs();
+	ArrayList<IPiste> GetListPistes();
+	IPiste GetPiste(String idPiste);
+	ArrayList<IPartie> GetListParties();
+	IPartie GetPartie(String idPartie);
+}
