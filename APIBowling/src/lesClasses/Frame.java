c13adeb5dd0c09bc3b739b53f9ad1fe824d50951
myc1986 <moratinos.c@gmail.com>
2014-05-29 19:18:15 +0200

    implémentation de toutes les méthodes sauf ObtenirScore et AfficherScore

--- APIBowling/src/lesClasses/Frame.java
+++ APIBowling/src/lesClasses/Frame.java
@@ -1,36 +1,120 @@
 package lesClasses;
 
+import java.util.ArrayList;
+
 import lesInterfaces.IFrame;
+import lesInterfaces.IPiste;
 
 public class Frame implements IFrame {
-
+	private ArrayList<String> _lesQuillesTombees;
+	private int _numero;
+	private boolean _termine;
+	private int _score;
+	private boolean _lancerSupplementaire;
+	
+	public Frame(int numeroFrame)
+	{
+		_numero = numeroFrame;
+		_termine = false;
+		_score = 0;
+		_lancerSupplementaire = false;
+		_lesQuillesTombees = new ArrayList<String>();
+	}
+	
 	@Override
-	public void MisAJourFrame(Piste unePiste) {
-		// TODO Auto-generated method stub
+	public void MiseAJour(IPiste unePiste) {
 		
+		if((!_termine && !_lancerSupplementaire) || (_lancerSupplementaire && _termine))
+		{
+			if (_lesQuillesTombees.size() <1) {
+				switch (unePiste.CompterTotalQuillesRenverse()) 
+				{
+					case 0:
+						_lesQuillesTombees.add("-");
+						break;
+					case 10:
+						_lesQuillesTombees.add("X");
+						
+						if(_numero==10)
+						{
+							_lancerSupplementaire = true;
+							unePiste.Reinitialiser();
+						}
+						
+						_termine = true;
+					default:
+						_lesQuillesTombees.add(String.valueOf(unePiste.CompterQuillesRenverseAuLance()));
+						_score = _score + unePiste.CompterQuillesRenverseAuLance();
+						break;
+				}
+			} else {
+				
+				if(_numero == 10 && _lesQuillesTombees.size()<=2)
+				{
+					switch (unePiste.CompterTotalQuillesRenverse()) 
+					{
+						case 0:
+							_lesQuillesTombees.add("-");
+							break;
+						case 10:
+							if(unePiste.CompterQuillesRenverseAuLance() == unePiste.CompterTotalQuillesRenverse())
+							{
+								_lesQuillesTombees.add("X");
+								unePiste.Reinitialiser();
+								_lancerSupplementaire = true;
+							}
+							else
+							{
+								_lesQuillesTombees.add("/");
+								_score = _score + unePiste.CompterQuillesRenverseAuLance();
+								_lancerSupplementaire = false;
+							}
+							
+							_termine = true;
+						default:
+							_lesQuillesTombees.add(String.valueOf(unePiste.CompterQuillesRenverseAuLance()));
+							_score = _score + unePiste.CompterQuillesRenverseAuLance();
+							break;
+					}
+				}
+				else
+				{
+					switch (unePiste.CompterTotalQuillesRenverse()) 
+					{
+						case 0:
+							_lesQuillesTombees.add("-");
+							break;
+						case 10:
+								_lesQuillesTombees.add("/");
+								_score = _score + unePiste.CompterQuillesRenverseAuLance();
+						default:
+							_lesQuillesTombees.add(String.valueOf(unePiste.CompterQuillesRenverseAuLance()));
+							_score = _score + unePiste.CompterQuillesRenverseAuLance();
+							break;
+					}
+				}
+			}
+		}
 	}
 
 	@Override
 	public boolean PeutEncoreLance() {
-		// TODO Auto-generated method stub
-		return false;
+		return (!_termine && !_lancerSupplementaire) || (_lancerSupplementaire && _termine);
 	}
 
 	@Override
-	public int GetNumeroFrame() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int GetNumero() {
+		return _numero;
 	}
 
 	@Override
 	public int ObtenirScore() {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public void AfficherScore() {
-		// TODO Auto-generated method stub
 		
 	}
 
