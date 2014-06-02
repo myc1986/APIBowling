ac85b12060a809496ca0f47185a9270b2ea23ac4
myc1986 <moratinos.c@gmail.com>
2014-06-02 16:29:04 +0200

    Implémentation des méthodes et fonctions de l'interface IJoueur

--- APIBowling/src/lesClasses/Joueur.java
+++ APIBowling/src/lesClasses/Joueur.java
@@ -1,25 +1,50 @@
 package lesClasses;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import lesInterfaces.IFrame;
 import lesInterfaces.IJoueur;
 
 public class Joueur implements IJoueur {
-
+	private String _prenom;
+	private ArrayList<IFrame> _mesFrames;
+	
 	@Override
 	public int ScoreTotalScore() {
-		// TODO Auto-generated method stub
+		int scoreTotal = 0;
+		
+		for (IFrame uneFrame : _mesFrames) {
+			scoreTotal = scoreTotal + uneFrame.ObtenirScore();
+		}
+		
 		return 0;
 	}
 
 	@Override
 	public String GetPrenom() {
-		// TODO Auto-generated method stub
-		return null;
+		return _prenom;
 	}
 
 	@Override
-	public int SetPrenom(String prenom) {
-		// TODO Auto-generated method stub
-		return 0;
+	public void SetPrenom(String prenom) {
+		_prenom = prenom;
 	}
 
+	private IFrame ObtenirFrameAJouer()
+	{
+		IFrame laFrameAJoue = null;
+		Integer iCountFrame = 0;
+		boolean aTrouve = false;
+		
+		do {
+			if(_mesFrames.get(iCountFrame).PeutEncoreLance())
+			{
+				laFrameAJoue = _mesFrames.get(iCountFrame);
+				aTrouve = true;
+			}
+		} while (iCountFrame >= _mesFrames.size() || aTrouve);
+		
+		return laFrameAJoue;
+	}
 }
